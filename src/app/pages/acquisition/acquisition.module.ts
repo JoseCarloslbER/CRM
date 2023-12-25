48b0cc7876028739f64201227be66797ee2b929b
Jose Lopez B <jose.briseno@eon.com.mx>
2023-12-25 17:38:32 -0600

    Etapa 3, modulo de clientes, corizaciones

--- src/app/pages/acquisition/acquisition.module.ts
+++ src/app/pages/acquisition/acquisition.module.ts
@@ -7,11 +7,18 @@ import { ACQUISITION_ROUTES } from './acquisition.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { MaterialModule } from 'app/shared/material/material.module';
+import { DetailClientComponent } from './companies/detail-client/detail-client.component';
+import { ModalNewActivityComponent } from './companies/modal-new-activity/modal-new-activity.component';
 
 
 
 @NgModule({
-  declarations: [AcquisitionComponent, CompaniesComponent, ModalNewCompanyComponent],
+  declarations: [
+    AcquisitionComponent, 
+    CompaniesComponent, 
+    ModalNewCompanyComponent, 
+    DetailClientComponent,
+    ModalNewActivityComponent],
   imports: [
     CommonModule,
     ACQUISITION_ROUTES,
