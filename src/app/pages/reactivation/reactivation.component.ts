e9648900ad865b9a31e716af00133cc09b7cca76
Jose Lopez B <jose.briseno@eon.com.mx>
2024-01-04 19:21:56 -0600

    modulo

---
+++ src/app/pages/reactivation/reactivation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reactivation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reactivation.component.html',
+  styleUrl: './reactivation.component.scss'
+})
+export class ReactivationComponent {
+
+}
