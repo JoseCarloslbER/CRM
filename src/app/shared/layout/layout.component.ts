f7af80db4e2b1d18c66a1497bdede8db8e1e4680
Jose Lopez B <jose.briseno@eon.com.mx>
2024-01-10 20:12:12 -0600

    admin funcionando

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -219,12 +219,6 @@ export class LayoutComponent implements OnInit, OnDestroy {
                             link : '/home/configuracion/categoria-producto'
                         },
                         {
-                            id   : '5',
-                            title: 'Categorias de campañas',
-                            type : 'basic',
-                            link : '/home/configuracion/categoria-campaña'
-                        },
-                        {
                             id   : '4',
                             title: 'Empresas',
                             type : 'basic',
